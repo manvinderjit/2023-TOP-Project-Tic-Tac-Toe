e026a17fb6b512ca5013f8100dba8ce292048bde
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-27 16:18:42 -0400

    refact: refactored gameboard generation

--- script.js
+++ script.js
@@ -9,23 +9,35 @@ const PlayerFactory = (name, marker) => {
     return { name, marker, age };
 }
 
-const displayController = (() => {
+const DisplayController = (() => {
 
     const gameboardContainer = document.getElementById('gameboard-container');
 
     const generateGrid = () => {
-        for (let i = 1 ; i <= 9; i++) {
-            let gamebox = document.createElement('div');
-            gamebox.classList.add('gamebox');
-            gamebox.id = i;
-            gameboardContainer.appendChild(gamebox);
+        // for (let i = 1 ; i <= 9; i++) {
+        //     let gamebox = document.createElement('div');
+        //     gamebox.classList.add('gamebox');
+        //     gamebox.id = i;
+        //     gamebox.textContent = 'X';
+        //     gameboardContainer.appendChild(gamebox);
+        // }
+
+        for (let i = 1 ; i <= 3; i++) {
+            for (let j = 1 ; j <= 3; j++) {
+                let gamebox = document.createElement('div');
+                gamebox.classList.add('gamebox');
+                gamebox.id = i + '-' + j;
+                gamebox.textContent = 'X';
+                gameboardContainer.appendChild(gamebox);
+            }
         }
     }
 
     return { generateGrid };
+
 })();
 
-displayController.generateGrid();
+DisplayController.generateGrid();
 
 
 const gameFlow = {};
